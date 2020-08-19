a018b0bc6b33602f512c8c66eff2bc1fef5c2794
Amine Azariz <amine@azariz.com>
2020-08-20 00:07:11 +0100

    New Section integration - NOS FORCES

---
+++ website/forces.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php $pageName = "Nos forces | ARMA Environnement" ?>
+		<?php include 'layout/head.php';?>
+	</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="90">
+		<!--map sec-->
+		<div class="main">
+			<div class="row no-gutters">
+				<!-- Side nav -->
+				<?php include 'layout/sideNav.php';?>
+				<!-- End side nav -->
+				<div id="page-wrapper">
+					<div class="overlay">
+						<div class="overlay-inner">
+							<img src="assets/img/overlays/1.jpg" alt="background">
+							<img src="assets/img/overlays/2.jpg" alt="background">
+							<img src="assets/img/overlays/3.jpg" alt="background">
+							<img src="assets/img/overlays/4.jpg" alt="background">
+							<img src="assets/img/overlays/7.jpg" alt="background">
+							<img src="assets/img/overlays/10.jpg" alt="background">
+						</div>
+						<div class="overlay-inner bg-secondary"></div>
+					</div>
+					<div class="content-area">
+						<div class="logo"><a href="index.html">
+						<img src="assets/img/logo-light.png" alt="ARMA Environnement"></a></div>
+						<div class="page row no-gutters">
+							<div class="col-md-9 offset-md-1">
+								<nav aria-label="breadcrumb">
+									<ol class="breadcrumb">
+										<li class="breadcrumb-item"><a href="index.html">ARMA Environnement</a></li>
+										<li class="breadcrumb-item active" aria-current="page">Nos forces</li>
+									</ol>
+								</nav>
+								<h1 class="page-title">Pourquoi choisir ARMA</h1>
+								<div class="text-bloc content-wrapper no-bg col-md-10 sub-h1">
+									<p>Pour ARMA Environnement, la qualité est un engagement
+										de chaque instant. Le bien-être des habitants est une priorité absolue qui guide chacune de nos actions. Ainsi, notre objectif est de devenir e partenaire de référence des communes et de devancer
+										leurs attentes en matière d’environnement, de services
+									et d’aménagements urbains.</p>
+								</div>
+								<div class="row content-bloc wow fadeIn">
+									<div class="col-lg-6 matchHeight">
+										<div class="small-text-bloc text-only">
+											<div class="text">
+												<h4 id="s1">
+												<b>Equipe qualifiée et à l’écoute</b>
+												<i class="fas fa-assistive-listening-systems"></i>
+												</h4>
+												<ul>
+													<li>Equipe hautement qualifiée orientée client</li>
+													<li>Communication fluide</li>
+													<li>Respect des engagements</li>
+													<li>Démarche de conseil et accompagnement personnalisé</li>
+												</ul>
+											</div>
+											<hr />
+											<div class="text">
+												<h4 id="s2">
+												<b>Expertise et savoir-faire éprouvés</b>
+												<i class="fas fa-globe-africa"></i>
+												</h4>
+												<ul>
+													<li>Engagée auprès de ses clients depuis 2012</li>
+													<li>Sources d’expertises diversifiées</li>
+													<li>Connaissance approfondie du Maroc</li>
+													<li>Partenaire des villes avec une compréhension fine de leurs enjeux</li>
+													<li>Intégration du développement durable dans chacun de nos métiers </li>
+												</ul>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6  matchHeight">
+										<div class="holder-parent">
+											<div class="image-holder" style="background-image: url('assets/img/content/forces-1.jpg');">
+												<img class="d-block d-lg-none" src="assets/img/content/3.jpg" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row content-bloc wow fadeIn reverse-mobile">
+									<div class="col-lg-6 matchHeight">
+										<div class="holder-parent">
+											<div class="image-holder"  style="background-image: url('assets/img/content/forces-2.jpg');">
+												<img class="d-block d-lg-none" src="assets/img/content/4.jpg" alt="">
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6 matchHeight">
+										<div class="small-text-bloc text-only">
+											<div class="text">
+												<h4 id="s3">
+												<b>Agilité et force de proposition</b>
+												<i class="fas fa-bezier-curve"></i>
+												</h4>
+												<ul>
+													<li>Adaptation aux différents enjeux et contextes</li>
+													<li>Anticipation des besoins</li>
+													<li>Capacité à mettre en perspectives et à réagir </li>
+													<li>Force de frappe capable d’intervenir à tout moment et sur tout le territoire</li>
+												</ul>
+											</div>
+											<hr />
+											<div class="text">
+												<h4 id="s4">
+												<b>Innovation à la pointe des dernières technologies</b>
+												<i class="fas fa-map-marked-alt"></i>
+												</h4>
+												<ul>
+													<li>Souci constant d’innovation et performance des équipements </li>
+													<li>Outils d’informations en temps réel</li>
+													<li>Service de suivi géographique (SIG) et pilotage cartographique en temps réel et différé</li>
+													<li>Relevé GPS des parcours accompagnant les prestations </li>
+													<li>Indicateurs précis de tournées de collecte et nettoiement relevés par GPS</li>
+													<li>Exécution par GPS de toute prestation de service </li>
+													<li>ARMA Complaint Management: application de suivi des demandes et réclamations clients </li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="row content-bloc wow fadeIn">
+									<div class="col-lg-6 matchHeight">
+										<div class="small-text-bloc text-only">
+											<div class="text">
+												<h4 id="s5">
+												<b>Qualité reconnue</b>
+												<i class="fas fa-medal"></i>
+												</h4>
+												<ul>
+													<li>Niveau de qualité aligné aux standards internationaux </li>
+													<li>Démarche d’amélioration continue </li>
+													<li>Respect des normes d’hygiène, qualité, sécurité et environnement</li>
+												</ul>
+											</div>
+											<hr />
+											<div class="text">
+												<h4 id="s6">
+												<b>Organisation optimisée </b>
+												<i class="fas fa-tasks"></i>
+												</h4>
+												<ul>
+													<li>Organisation dédiée en mode projet </li>
+													<li>Système de suivi et de contrôle du service</li>
+													<li>Gestion optimale des ressources</li>
+													<li>Digitalisation des process</li>
+													<li>Stratégie de développement de nouveaux métiers</li>
+													<li>Management de la maintenance par GMAO</li>
+
+												</ul>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-6  matchHeight">
+										<div class="holder-parent">
+											<div class="image-holder" style="background-image: url('assets/img/content/forces-3.jpg');">
+												<img class="d-block d-lg-none" src="assets/img/content/3.jpg" alt="">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php include 'layout/footer.php';?>
+	</body>
+</html>
