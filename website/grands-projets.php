fd8cfb755ade5678345fa8db34d82f4567d2fa70
msaji <mohamed.saji@viseo.com>
2021-01-30 14:26:09 +0100

    grands projets

---
+++ website/grands-projets.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php $pageName = "Nos grands projets | ARMA Environnement" ?>
+	<?php include 'layout/head.php';?>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="90">
+
+	<!--map sec-->
+	<div class="main one-pager">
+		<div class="row no-gutters">
+			<!-- Side nav -->
+			<?php include 'layout/sideNav.php';?>
+			<!-- End side nav -->
+			<div id="page-wrapper">				
+				<div class="content-area">					
+					<section class="section full bg-blue white-text text-center">
+						<div class="container">
+							<h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">ARMA ENVIRONNEMENT</h2>
+							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s">Nos plus grands projets et réalisations</h3>
+							<p class="mb-50 wow animated zoomIn" data-wow-delay=".4s">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+						</div>
+                    </section>
+                    <section class="section white-text text-center full-bg" style="background-image: url(assets/img/grands-projets/cover-casablanca.png)">
+                        <div class="container">
+                            <h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">NOS GRANDS PROJETS</h2>
+							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s">Propreté de la métropole Casablanca</h3>
+                        </div>
+                    </section>
+                    <section class="section bg-white">
+                        <div class="container">
+                            <h3 class="section-subtitle mb-md-5 wow animated fadeInLeft">ARMA Environnement <br>assure la propreté de<br> la métropole, <br>Casablanca.</h3>
+                            <div class="row">  
+                                <div class="col-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".2s">                                      
+                                    <p>Depuis son démarrage officiel en 2019, le projet de gestion de la propreté de la ville de Casablanca est certainement l’un des plus grands projets d’ARMA ENVIRONNEMENT, étant donné que la capitale économique est bel et bien la plus grande ville du Maroc. </p>
+                                    <p>Avec un effectif global des plus importants ARMA CASABLANCA offre ainsi ses services de propreté urbaine à 4 préfectures d’arrondissements :</p>
+                                    <div class="img-holder">
+                                        <img src="assets/img/grands-projets/map-casa.png" alt="Map casa">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 px-md-5 wow animated fadeInRight" data-wow-delay=".2s">
+                                    <p>Le projet a connu la mise en œuvre des meilleurs systèmes de suivi et de contrôle de prestation digitalisés et une performance technique et environnementale entièrement géolocalisée, ainsi que l’adoption de management de la maintenance par l’utilisation GMAO.</p>
+                                    <div class="section-stats">
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">1.5</span> millions                                           
+                                            </span>
+                                            habitants desservis
+                                        </div>
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">72</span> km<sup>2</sup>                                           
+                                            </span>
+                                            de superficie entretenue par nos équipes
+                                        </div>
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">440000</span> t/an                                           
+                                            </span>
+                                            de déchets collectés
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="section white-text text-center full-bg" style="background-image: url(assets/img/grands-projets/cover-rabat.png)">
+                        <div class="container">
+                            <h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">NOS GRANDS PROJETS</h2>
+							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s">Collecte des déchets de la ville de Rabat</h3>
+                        </div>
+                    </section>
+                    <section class="section bg-white">
+                        <div class="container">
+                            <h3 class="section-subtitle mb-md-5 wow animated fadeInLeft">ARMA Environnement <br>collecte les déchets de<br> la capitale du Maroc,<br>Rabat.</h3>
+                            <div class="row">  
+                                <div class="col-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".2s">                                      
+                                    <p>Depuis 2015, ARMA RABAT assure la collecte des déchets ménagers et assimilés de la ville de Rabat. Ce contrat d’une durée de 7 ans concerne 3 principaux arrondissement de la capitale, Hassan, Yaacoub Al Mansour et Touarga.</p>
+                                    <p>Ce sont 250 salariés engagés au quotidien afin de réussir cette noble mission.</p>
+                                    <div class="img-holder mt-5">
+                                        <img src="assets/img/grands-projets/rabat.png" alt="Rabat">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 px-md-5 wow animated fadeInRight" data-wow-delay=".2s">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                                    <div class="section-stats">
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">7</span>  ans                                           
+                                            </span>
+                                            contrat 2015-2022
+                                        </div>                                        
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">300000</span>                 
+                                            </span>
+                                            habitants desservis
+                                        </div>
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">26</span> km<sup>2</sup>                                           
+                                            </span>
+                                            de superficie entretenue
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="section white-text text-center full-bg" style="background-image: url(assets/img/grands-projets/cover-eclair-rabat.png)">
+                        <div class="container">
+                            <h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">NOS GRANDS PROJETS</h2>
+							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s">Maintenance de l'éclairage public de la ville de Rabat</h3>
+                        </div>
+                    </section>
+                    <section class="section bg-white">
+                        <div class="container">
+                            <h3 class="section-subtitle mb-md-5 wow animated fadeInLeft">ARMA Energy illumine<br>la ville lumière, Rabat.</h3>
+                            <div class="row">  
+                                <div class="col-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".2s">                                      
+                                    <p>ARMA ENERGY assure depuis avril 2018 l’entretien ainsi que la maintenance préventive et curative de l’éclairage public de la ville de Rabat, avec plus de 45000 points lumineux entretenus.</p>
+                                    <p>Animée d’une recherche constante de l’innovation et du développement durable, ARMA ENERGY propose des solutions permettant d’améliorer la qualité de service et l’efficacité énergétique. Aujourd’hui, ARMA ENERGY s’engage sur un taux de disponibilité atteignant les 98%.</p>
+                                    <div class="img-holder mt-5">
+                                        <img src="assets/img/grands-projets/img-eclair-rabat.png" alt="Rabat">
+                                    </div>
+                                </div>
+                                <div class="col-md-6 px-md-5 wow animated fadeInRight" data-wow-delay=".2s">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                                    <div class="section-stats">
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">2018</span>                                           
+                                            </span>
+                                            à aujourd’hui...
+                                        </div>                                        
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                +<span class="number-scroll">45000</span>                 
+                                            </span>
+                                            points lumineux
+                                        </div>
+                                        <div class="section-stat">
+                                            <span class="number">
+                                                <span class="number-scroll">98</span>%          
+                                            </span>
+                                            de taux de disponibilité
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include 'layout/footer.php';?>
+</body>
+</html>
