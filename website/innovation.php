e05adbc2fdd2c3ea114fa70f25e1efac32dd650a
Amine Azariz <amine@azariz.com>
2021-02-15 18:55:07 +0100

    Innov page fixes + new content

--- website/innovation.php
+++ website/innovation.php
@@ -14,12 +14,15 @@
 			<!-- End side nav -->
 			<div id="page-wrapper" class="innov-page">				
 				<div class="content-area">
-					<section class="section white-text full-bg after mystaff" style="background-image: url(assets/img/innovation.png)">
+
+                    <!-- App 1 -->
+
+					<section id="staff" class="section white-text full-bg after mystaff" style="background-image: url(assets/img/innovation.png)">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-7">
                                     <h2 class="section-title wow animated zoomIn" data-wow-delay=".2s">NOS INNOVATIONS</h2>
-                                    <h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s"><img src="assets/img/icons/mystaff.png" alt="My staff" width="290px"></h3>
+                                    <h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s"><img class="innov-logo" src="assets/img/icons/mystaff.png" alt="MyStaff" height="50"></h3>
                                     <p class="wow animated zoomIn pl-4" data-wow-delay=".6s">Le logiciel de gestion développé par ARMA pour centraliser les données d’assiduité et de performance de son personnel.</p>
                                 </div>
                             </div>    
@@ -28,7 +31,7 @@
                     </section>
                     <section class="section bg-white">
                         <div class="container">
-                            <h3 class="section-subtitle innov-subtitle wow animated fadeInLeft" data-wow-delay=".2s">Une gestion de l'assiduité orientée performance.</h3>
+                            <h3 class="section-subtitle innov-subtitle wow animated fadeInLeft" data-wow-delay=".2s">Une gestion de l'assiduité orientée performance</h3>
                             <div class="row">
                                 <div class="col-md-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".4s">                                      
                                     <h4 class="small-title mt-0">Fonctionnalités</h4>
@@ -79,12 +82,15 @@
                             </div>
                         </div>
                     </section>
-                    <section class="section white-text full-bg after ask" style="background-image: url(assets/img/innovation.png)">
+
+                    <!-- App 2 -->
+
+                    <section id="ask" class="section white-text full-bg after ask" style="background-image: url(assets/img/innovation.png)">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-7">
                                     <h2 class="section-title wow animated zoomIn" data-wow-delay=".2s">NOS INNOVATIONS</h2>
-                                    <h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s"><img src="assets/img/icons/ask-logo.png" alt="Ask App" width="135px"></h3>
+                                    <h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s"><img class="innov-logo" src="assets/img/icons/ask-logo.png" alt="Ask" height="50"></h3>
                                     <p class="innov-title-desc wow animated zoomIn pl-4" data-wow-delay=".6s">Une application responsive et adaptée à tous les supports qui permet la saisie l’enregistrement et la gestion des réclamations adressées aux différentes filiales du Groupe ARMA.</p>
                                 </div>                                
                             </div> 
@@ -93,7 +99,7 @@
                     </section>
                     <section class="section bg-white">
                         <div class="container">
-                            <h3 class="section-subtitle innov-subtitle wow animated fadeInLeft" data-wow-delay=".2s">Une gestion avancée réclamations, à l'ère du digital.</h3>
+                            <h3 class="section-subtitle innov-subtitle wow animated fadeInLeft" data-wow-delay=".2s">Une gestion avancée des réclamations, à l'ère du digital</h3>
                             <div class="row">
                                 <div class="col-md-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".4s">
                                     <h4 class="small-title mt-0">Fonctionnalités</h4>
@@ -155,6 +161,90 @@
                             </div>
                         </div>
                     </section>
+
+                    <!-- App 3 -->
+
+                    <section id="clean" class="section white-text full-bg after ask" style="background-image: url(assets/img/innovation.png)">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <h2 class="section-title wow animated zoomIn" data-wow-delay=".2s">NOS INNOVATIONS</h2>
+                                    <h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s"><img class="innov-logo" src="assets/img/icons/cleanplus-logo.png" alt="CLEAN+" height="50"></h3>
+                                    <p class="innov-title-desc wow animated zoomIn pl-4" data-wow-delay=".6s">  
+   Application permettant le traitement en temps réel de tous les problèmes et réclamations effectuées en interne. Nos contrôleurs terrain peuvent signaler des anomalies ponctuelles à tout moment en utilisant CLEAN+, d’en prendre une photo, de partager l’anomalie sur une application qui permettra ensuite à celle-ci d’être traitée.
+</p>
+                                </div>                                
+                            </div> 
+                            <img class="img-after" src="assets/img/content/innov-1.png" alt="Ask App">                           
+                        </div>
+                    </section>
+                    <section class="section bg-white">
+                        <div class="container">
+                            <h3 class="section-subtitle innov-subtitle wow animated fadeInLeft" data-wow-delay=".2s">Une supérvision et contrôle terrain à la pointe de la tech</h3>
+                            <div class="row">
+                                <div class="col-md-6 px-md-5 wow animated fadeInLeft" data-wow-delay=".4s">
+                                    <h4 class="small-title mt-0">Fonctionnalités</h4>
+                                    <ul>
+                                        <li>Saisie de votre réclamation en toute simplicité</li>
+                                        <li>Prise de photo géolocalisée de l’anomalie</li>
+                                        <li>Affectation automatique au responsable de la zone pour traitement</li>
+                                        <li>Suivi de la requête depuis sa création jusqu’à sa clôture</li>
+                                        <li>Analyse des réclamations grâce au DASHBOARD de l’application</li>
+                                    </ul>
+                                    <div class="app-gallery my-3 gallery-2">
+                                        <a href="assets/img/content/innov-4.png"><img src="assets/img/content/innov-4.png" alt="Ask App"></a>
+                                        <a href="assets/img/content/innov-4.png"><img src="assets/img/content/innov-4.png" alt="Ask App"></a>
+                                        <a href="assets/img/content/innov-4.png"><img src="assets/img/content/innov-4.png" alt="Ask App"></a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 px-md-5 wow animated fadeInRight" data-wow-delay=".2s">
+                                    <h4 class="small-title mt-0">LES + DE l'application</h4>
+                                  
+                                    <ul class="advatages">
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/fast.png" alt="Rapidité">
+                                            </div>
+                                            <p>Prise en charge rapide des demandes et réclamations formulées.</p>
+                                        </li>
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/calendar.png" alt="Temps">
+                                            </div>
+                                            <p>Réduction considérable de la durée de traitement des demandes.</p>
+                                        </li>
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/improvment.png" alt="Amélioration">
+                                            </div>
+                                            <p>Amélioration de la réactivité et de la qualité de traitement des demandes reçues.</p>
+                                        </li>
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/responsibility.png" alt="Responsabilisation">
+                                            </div>
+                                            <p>Responsabilisation des collaborateurs selon le circuit attribué.</p>
+                                        </li>
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/help.png" alt="Aide à la prise de décision">
+                                            </div>
+                                            <p>Augmentation de la transparence des opérations par des alertes automatisées.</p>
+                                        </li>
+                                        <li>
+                                            <div class="icon-img">
+                                                <img src="assets/img/small-logos/easy.png" alt="Facilitation">
+                                            </div>
+                                            <p>Meilleure maitrise du parcours client et amélioration de sa satisfaction.</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+
+
 				</div>
 			</div>
 		</div>
