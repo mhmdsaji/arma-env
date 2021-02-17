0bf453db353fc6ea68c43eb3dfd561707e4d3168
Amine Azariz <amine@azariz.com>
2021-02-17 14:24:51 +0100

    Jobs content

--- website/jobs.php
+++ website/jobs.php
@@ -17,51 +17,51 @@ if (isset($_POST["uid"])) {    // the user has submitted the form
 	$formsubmit = true;
   // Check if the "from" input field is filled out
 	if ($_POST["messagebody"] && 
-			$_POST["fullname"] && 
-			$_POST["phone"] && 
-			$_POST["objectemail"] && 
-			$_FILES["cvattach"] && 
-			$_POST["address"]) {
+		$_POST["fullname"] && 
+		$_POST["phone"] && 
+		$_POST["objectemail"] && 
+		$_FILES["cvattach"] && 
+		$_POST["address"]) {
 		$uid = uniqid();
-		$objectemail = $_POST["objectemail"];
-		$subject = "Candidature [$uid] - $objectemail";
-		$messagebody = $_POST["messagebody"];
-		$address = $_POST["address"];
-		$phone = $_POST["phone"];
-		$fullname = $_POST["fullname"];
-		$sitesource = $_POST["sitesource"];
+	$objectemail = $_POST["objectemail"];
+	$subject = "Candidature [$uid] - $objectemail";
+	$messagebody = $_POST["messagebody"];
+	$address = $_POST["address"];
+	$phone = $_POST["phone"];
+	$fullname = $_POST["fullname"];
+	$sitesource = $_POST["sitesource"];
 			// message lines should not exceed 70 characters (PHP rule), so wrap it
-		$texttomail = "Offre: $objectemail\r\nSite: $sitesource\r\nNom: $fullname\r\nNuméro de téléphone: $phone\r\nAdresse: $address\r\n\r\n$messagebody";
+	$texttomail = "Offre: $objectemail\r\nSite: $sitesource\r\nNom: $fullname\r\nNuméro de téléphone: $phone\r\nAdresse: $address\r\n\r\n$messagebody";
 			// send mail
 
-		$formok = true;
-		$errmsg = '';
-		$uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['cvattach']['name']));
-		if (move_uploaded_file($_FILES['cvattach']['tmp_name'], $uploadfile)) {
+	$formok = true;
+	$errmsg = '';
+	$uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['cvattach']['name']));
+	if (move_uploaded_file($_FILES['cvattach']['tmp_name'], $uploadfile)) {
 				// Upload handled successfully
 				// Now create a message
-			$mail = new PHPMailer();
-			$mail->isSendmail();
-			$mail->setFrom('noreply@arma.ma', 'ARMA Environnement');
-			$mail->addAddress($SEND_TO);
-			$mail->Subject = $subject;
-			$mail->Body = $texttomail;
+		$mail = new PHPMailer();
+		$mail->isSendmail();
+		$mail->setFrom('noreply@arma.ma', 'ARMA Environnement');
+		$mail->addAddress($SEND_TO);
+		$mail->Subject = $subject;
+		$mail->Body = $texttomail;
 				// Attach the uploaded file
-			$mail->addAttachment($uploadfile, $_FILES['cvattach']['name']);
-			if (!$mail->send()) {
-				$formok = false;
-				$errmsg .= "Mailer error: " . $mail->ErrorInfo;
-			}
-		} else {
+		$mail->addAttachment($uploadfile, $_FILES['cvattach']['name']);
+		if (!$mail->send()) {
 			$formok = false;
-			$errmsg .= 'Failed to move file to ' . $uploadfile;
+			$errmsg .= "Mailer error: " . $mail->ErrorInfo;
 		}
-
 	} else {
 		$formok = false;
-		$toto = $_FILES['cvattach'];
-		error_log("Blablabla... $toto", 0);
+		$errmsg .= 'Failed to move file to ' . $uploadfile;
 	}
+
+} else {
+	$formok = false;
+	$toto = $_FILES['cvattach'];
+	error_log("Blablabla... $toto", 0);
+}
 }
 ?>
 
@@ -83,15 +83,15 @@ if (isset($_POST["uid"])) {    // the user has submitted the form
 			<div id="page-wrapper">				
 				<div class="content-area">
 					<section class="section white-text text-center full-bg" style="background-image: url(assets/img/nous-rejoindre.png)">
-                        <div class="container">
-                            <h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">ARMA Enviornnement</h2>
+						<div class="container">
+							<h2 class="section-title wow animated fadeInLeft" data-wow-delay=".2s">ARMA Enviornnement</h2>
 							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".4s">NOUS REJOINDRE</h3>
 							<p class="wow animated zoomIn" data-wow-delay=".6s">Chez Le Groupe ARMA , quelle que soit votre mission, vous ferez la différence en concrétisant vos idées enthousiastes afin de créer de la valeur dans un environnement de travail propice au bien-être et à l’accomplissement de soi.</p>
-                        </div>
+						</div>
 					</section>
 					<section class="section bg-white text-center">
 						<div class="container">
-						<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".2s">Consultez nos offres d'emplois</h3>
+							<h3 class="section-subtitle wow animated zoomIn" data-wow-delay=".2s">Consultez nos offres d'emplois</h3>
 							<p class="wow animated zoomIn" data-wow-delay=".4s">La diversité, le développement de compétences, l’innovation et le bien-être au travail, sont les piliers de base de notre politique RH,  vous permettant ainsi de voir vos projets s’accomplir et de libérer votre potentiel tout en cultivant le leadership, l’excellence et l’agilité,  des valeurs clés qui guident nos actions et challenges au quotidien. Au sein du groupe ARMA, nous fédérons nos équipes autour d’un projet d’entreprise mobilisateur construit sur l’innovation, la création de valeur, l’engagement  sociétale, et le développement d’expertises. Vous vous y voyez déjà ? Envoyez votre candidature et osez l’expérience ARMA !</p>
 							<div class="grid-layout content-wrapper wow fadeIn mt-5" data-wow-delay=".6s">
 								<div class="card-deck mb-4">
@@ -100,126 +100,48 @@ if (isset($_POST["uid"])) {    // the user has submitted the form
 											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
 											<img class="star" src="assets/img/icons/star.png" alt="">
 										</div>
-										<a href="#">
-	                                        <div class="card-body">
-	                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-	                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-	                                        </div>
-	                                        <div class="card-footer">
-	                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-	                                        </div>
-                                    	</a>
+										<a href="content/jobs/job1.pdf">
+											<div class="card-body">
+												<h5 class="card-title">ADMINISTRATEUR APPLICATIF (SYSTEMES D'INFORMATION)</h5>
+												<h6 class="card-subtitle mb-2 text-muted">ARMA Environnement</h6>
+											</div>
+											<div class="card-footer">
+												<div class="card-date">LE 01/03/2021 — RABAT, MA</div>
+											</div>
+										</a>
 									</div>
 									<div class="card">
 										<div class="card-header">
 											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
 											<img class="star" src="assets/img/icons/star.png" alt="">
 										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
+										<a href="content/jobs/job2.pdf">
+											<div class="card-body">
+												<h5 class="card-title">RESPONSABLE DÉVELOPPEMENT RH (RESSOURCES HUMAINES)</h5>
+												<h6 class="card-subtitle mb-2 text-muted">ARMA Environnement</h6>
+											</div>
+											<div class="card-footer">
+												<div class="card-date">LE 01/03/2021 — RABAT, MA</div>
+											</div>
+										</a>
 									</div>
 									<div class="card">
 										<div class="card-header">
 											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
 											<img class="star" src="assets/img/icons/star.png" alt="">
 										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
+										<a href="content/jobs/job3.pdf">
+											<div class="card-body">
+												<h5 class="card-title">RESPONSABLE D’EXPLOITATION (EXPLOITATION)</h5>
+												<h6 class="card-subtitle mb-2 text-muted">ARMA Environnement</h6>
+											</div>
+											<div class="card-footer">
+												<div class="card-date">LE 01/03/2021 — RABAT, MA</div>
+											</div>
+										</a>
 									</div>	
-                                </div>
-                                <div class="card-deck mb-4">
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>	
-                                </div>
-                                <div class="card-deck mb-4">
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>
-									<div class="card">
-										<div class="card-header">
-											<img class="card-logo" src="assets/img/icons/logo.png" alt="Arma Environnement">
-											<img class="star" src="assets/img/icons/star.png" alt="">
-										</div>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Responsable Développement Ressources Humaines</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">ARMA Environnement, Rabat</h6>
-                                        </div>
-                                        <div class="card-footer">
-                                            <div class="card-date">LE 01 / 02 / 2021 </div>
-                                        </div>
-									</div>	
-                                </div>
-                            </div>
+								</div>
+							</div>
 						</div>
 					</section>
 					<section id="s2" class="section bg-white text-center pt-0">
@@ -244,8 +166,9 @@ if (isset($_POST["uid"])) {    // the user has submitted the form
 										<label for="objectemail">Offre de candidature*</label>
 										<select name="objectemail" id="objectemail" class="form-control">
 											<option value="Candidature spontanée">Candidature spontanée</option>
-											<option value="CRabat - Responsable Développement Ressources Humaines">Rabat - Responsable Développement Ressources Humaines</option>
-											<option value="Casa - Responsable exploitation">Casa - Responsable exploitation</option>
+											<option value="Job1">ADMINISTRATEUR APPLICATIF (SYSTEMES D'INFORMATION)</option>
+											<option value="Job2">RESPONSABLE DÉVELOPPEMENT RH (RESSOURCES HUMAINES)</option>
+											<option value="Job3">RESPONSABLE D’EXPLOITATION (EXPLOITATION)</option>
 										</select>
 									</div>
 
